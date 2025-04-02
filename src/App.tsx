1d2f91784d80cb64a1d39138961c2e31ba2d294f
AmeerZedany <ameer.zedane2@gmail.com>
2025-04-02 15:59:06 +0300

    Add homepage script to package.json and remove unused React import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Navbar from './components/Navbar';
